7282fb0a90ee0f4c1dd36dfb40a83102562ed020
ken <zhxbab@163.com>
2016-05-19 02:40:09 +0800

    add reg function

--- c_generator/csmith/csmith_1T_1T_protect_mode_30007/csmith_1T_0_1T_protect_mode_1533999530.asm
+++
@@ -1,4 +0,0 @@
-include "/media/Data_Linux/tools/tpg//include/std.inc";
-#### hlt_code: start addr is 0x34250 and size is 0x200
-#### int_handler_base: start addr is 0xcfbfc0 and size is 0x100000
-#### int_handler_record_base: start addr is 0x6b9f0 and size is 0x800
