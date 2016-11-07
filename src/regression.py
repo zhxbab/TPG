73e2b65af3df8e460ba3d11367717d72b2602920
ken <zhxbab@163.com>
2016-11-07 21:00:15 +0800

    change time

--- src/regression.py
+++ src/regression.py
@@ -265,11 +265,11 @@ class Regression(Util):
         runpclmsi_p_ret = self.Do_runpclmsi(runpclmsi_cmd,self.runpclmsi_time)
         if runpclmsi_p_ret == 0x0:
             Info("PCLMSI SUCCESSFULLY",self.freglog)
+            time.sleep(2)
             if not self.skip_check_fail:
                 result = self.Parse_pclmsi_log_jtrk("%s.jtrk"%(self.base_name))
             if result:
                 Info("Find Fail Sleep 5s!",self.freglog)
-                time.sleep(5)
                 return result
         else:
             time.sleep(3)
