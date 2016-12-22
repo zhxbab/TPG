b3dc0f4f6f7bfb6d407fac5406337aca3abea3bb
ken <zhxbab@163.com>
2016-12-22 10:59:00 +0800

    modify note

--- clib/template/template.c
+++ clib/template/template.c
@@ -1,8 +1,12 @@
-//#include "../include/stdio.h"
-#include "../include/string.h"
+#include <stdlib.h>
+#include <string.h>
 void main(){
 	const char *str = "Hello World!";
 	char  des[20];
+	const char *str1 = "65";
+	int a = 0;
 	strcpy(des,str);
+	a = atoi(str1);
+	
 }
 
